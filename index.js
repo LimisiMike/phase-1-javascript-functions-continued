ab5f861a54b6920c7e2d850ebb9c243612463609
Michael <michaellimisi7@gmail.com>
2023-03-18 21:45:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// code your solution here
+// function saturdayFun(){
+
+// }
+
+
+function saturdayFun(activity ='roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+   }
+   saturdayFun();
+   saturdayFun('bathe my dog');
+
+
+//    Func
+// const work = function mondayWork() {
+
+//    }
+
+function mondayWork(activity="go to the office") {
+       return `This Monday, I will ${activity}.`
+   }
+   mondayWork();
+   mondayWork("work from home");
+
+
+//    WrapAdject fun
+function wrapAdjective(string="*") {
+  return function(parameter="special") {
+    return `You are ${string}${parameter}${string}!`
+  }
+}
